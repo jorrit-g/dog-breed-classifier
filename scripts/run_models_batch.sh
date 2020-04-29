71e27ff712e008f8919837f7b5aa9842d1152113
jorrit <jorrit@dieselboris.nl>
2020-04-29 13:19:55 +0200

    run scripts now in separate folder, run output in output folder

--- scripts/run_models_batch.sh
+++ scripts/run_models_batch.sh
@@ -8,7 +8,9 @@
 #          Please note output from each run has been piped into a text file.
 #
 # Usage: sh run_models_batch.sh    -- will run program from commandline within Project Workspace
-#  
-python check_images.py --dir pet_images/ --arch resnet  --dogfile dognames.txt > resnet_pet-images.txt
-python check_images.py --dir pet_images/ --arch alexnet --dogfile dognames.txt > alexnet_pet-images.txt
-python check_images.py --dir pet_images/ --arch vgg  --dogfile dognames.txt > vgg_pet-images.txt
+#
+rm -rf "output"
+mkdir "output"
+python check_images.py --dir pet_images/ --arch resnet  --dogfile dognames.txt > output/resnet_pet-images.txt
+python check_images.py --dir pet_images/ --arch alexnet --dogfile dognames.txt > output/alexnet_pet-images.txt
+python check_images.py --dir pet_images/ --arch vgg  --dogfile dognames.txt > output/vgg_pet-images.txt
--- scripts/run_models_batch_uploaded.sh
+++ scripts/run_models_batch_uploaded.sh
@@ -8,7 +8,9 @@
 #          Please note output from each run has been piped into a text file.
 #
 # Usage: sh run_models_batch_uploaded.sh    -- will run program from commandline within Project Workspace
-#  
-python check_images.py --dir uploaded_images/ --arch resnet  --dogfile dognames.txt > resnet_uploaded-images.txt
-python check_images.py --dir uploaded_images/ --arch alexnet --dogfile dognames.txt > alexnet_uploaded-images.txt
-python check_images.py --dir uploaded_images/ --arch vgg  --dogfile dognames.txt > vgg_uploaded-images.txt
+#
+rm -rf "output"
+mkdir "output"
+python check_images.py --dir uploaded_images/ --arch resnet  --dogfile dognames.txt > output/resnet_uploaded-images.txt
+python check_images.py --dir uploaded_images/ --arch alexnet --dogfile dognames.txt > output/alexnet_uploaded-images.txt
+python check_images.py --dir uploaded_images/ --arch vgg  --dogfile dognames.txt > output/vgg_uploaded-images.txt
